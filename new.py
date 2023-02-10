3b75a2ab0b0ec620218d2a72bdad20218cfc13e0
dilliprakash <dilludeepu96@gmail.com>
2023-02-10 12:36:31 +0530

    initial commit

--- new.py
+++ new.py
@@ -1,15 +1,25 @@
+import os
 import openai
+import json
+import textwrap
+import re
+from time import time,sleep
+
+
 
 def open_file(filepath) :
     with open(filepath, 'r', encoding='utf-8') as infile:
         return infile.read()
         
-open.api_key = open_file('openaiapikey.txt')
+
+openai.api_key = open_file('openaiapikey.txt')
+
+
 
 
 def gpt3_completion(prompt, engine='test-davinci-002', temp=0.7, top_p=1.0, tokens=400, freq_pen=0.0, pres_pen=0.0, stop=['<<END>>']):
     prompt = prompt.encode(encoding='ASCII',errors='ignore').decode()
-    response = openai.completion.create(
+    response = openai.Completion.create(
         engine=engine,
         prompt=prompt,
         temperature=temp,
